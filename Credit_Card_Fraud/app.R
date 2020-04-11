43d6ebff01df8fd48a08c52a26cbb63f4bf25f0d
Derek-Funk <dfunk0923@gmail.com>
2020-04-11 18:43:46 -0400

    Add files via upload

--- Credit_Card_Fraud/app.R
+++ Credit_Card_Fraud/app.R
@@ -41,7 +41,7 @@ ui = dashboardPage(skin = "black",
                          icon = icon("brain")
                        ),
                        menuItem(
-                         text = "Decision Tree",
+                         text = "Random Forest",
                          tabName = "decisionTree",
                          icon = icon("bezier-curve")
                        ),
@@ -107,36 +107,160 @@ ui = dashboardPage(skin = "black",
                          )
                        ),
                        tabItem(
-                         tabName = "eda"
+                         tabName = "eda",
+                         tabsetPanel(
+                           tabPanel(
+                             title = "Correlations",
+                             uiOutput(outputId = "image_correlationMatrix")
+                           ),
+                           tabPanel(
+                             title = "Distributions",
+                             uiOutput(outputId = "image_overTime") %>% withSpinner(type = 6, color = "#76B7D2"),
+                             uiOutput(outputId = "image_classDistribution") %>% withSpinner(type = 6, color = "#76B7D2")
+                           )
+                         )
                        ),
                        tabItem(
                          tabName = "architecture",
-                         imageOutput(outputId = "image_architecture")
+                         uiOutput(outputId = "image_architecture")
                        ),
                        tabItem(
-                         tabName = "sagemaker"
+                         tabName = "sagemaker",
+                         tabsetPanel(
+                           tabPanel(
+                             title = "Precision & Recall",
+                             uiOutput(outputId = "image_recall") %>% withSpinner(type = 6, color = "#76B7D2"),
+                             uiOutput(outputId = "image_recallAndPrecision") %>% withSpinner(type = 6, color = "#76B7D2")
+                           )
+                         )
                        ),
                        tabItem(
                          tabName = "decisionTree",
-                         selectInput(inputId = "treeNumber", label = "Decision Tree", choices = 0:99),
-                         uiOutput(outputId = "image_decisionTrees",
-                                  style = "height:800px; width:1400px; overflow-y: scroll;overflow-x: scroll;")
+                         tabsetPanel(
+                           tabPanel(
+                             title = "Model Validation",
+                             column(
+                               width = 8,
+                               uiOutput(outputId = "image_randomForest_confusionMatrix")
+                             ),
+                             column(
+                               width = 4,
+                               HTML("
+                <h4>Accuracy Score = 0.999438</h4><br>   
+                <h4>Precision Score = 0.934211</h4><br>
+                <h4>Recall Score = 0.72449</h4><br>
+                <h4>F1 Score = 0.816092</h4><br>
+              "),
+                               br(),
+                               uiOutput(outputId = "image_randomForest_roc")
+                             )
+                           ),
+                           tabPanel(
+                             title = "Individual Trees",
+                             selectInput(inputId = "treeNumber", label = "Decision Tree", choices = 0:99),
+                             uiOutput(outputId = "image_decisionTrees",
+                                      style = "height:800px; width:1400px; overflow-y: scroll;overflow-x: scroll;")
+                           )
+                         )
                        ),
                        tabItem(
                          tabName = "logisticRegression",
-                         #temp
-                         #####
-                         column(width = 6,
-                                uiOutput(outputId = "modelVisual2_1"),
-                                uiOutput(outputId = "modelVisual2_2")
-                         ),
-                         column(width = 6,
-                                sliderInput(inputId = "x1", label = "X1", min = 0, max = 0.3, value = 0.15, step = 0.01),
-                                sliderInput(inputId = "x2", label = "X2", min = 0, max = 50, value = 25, step = 1),
-                                sliderInput(inputId = "x3", label = "X3", min = 0, max = 50, value = 25, step = 1),
-                                uiOutput(outputId = "logOdds")
+                         tabsetPanel(
+                           tabPanel(
+                             title = "Model Validation",
+                             column(
+                               width = 8,
+                               uiOutput(outputId = "image_logistic_confusionMatrix")
+                             ),
+                             column(
+                               width = 4,
+                               HTML("
+                <h4>Accuracy Score = 0.928934</h4><br>   
+                <h4>Precision Score = 0.966667</h4><br>
+                <h4>Recall Score = 0.887755</h4><br>
+                <h4>F1 Score = 0.925532</h4><br>
+              "),
+                               br(),
+                               uiOutput(outputId = "image_logistic_roc")
+                             )
+                           ),
+                           tabPanel(
+                             title = "Estimate Tool",
+                             fluidRow(
+                               box(
+                                 title = "Logistic Regression Overview",
+                                 width = 6,
+                                 height = "400px",
+                                 uiOutput(outputId = "logistic_overview")
+                               ),
+                               box(
+                                 title = "Parameters",
+                                 width = 6,
+                                 height = "400px",
+                                 # br(),
+                                 DT::dataTableOutput(outputId = "dataTable_logisticCoefficients")
+                               )
+                             ),
+                             fluidRow(
+                               box(
+                                 title = "Estimates",
+                                 width = 6,
+                                 height = "450px",
+                                 column(
+                                   width = 2,
+                                   lapply(1:5, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   })
+                                 ),
+                                 column(
+                                   width = 2,
+                                   lapply(6:10, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   })
+                                 ),
+                                 column(
+                                   width = 2,
+                                   lapply(11:15, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   })
+                                 ),
+                                 column(
+                                   width = 2,
+                                   lapply(16:20, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   })
+                                 ),
+                                 column(
+                                   width = 2,
+                                   lapply(21:25, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   })
+                                 ),
+                                 column(
+                                   width = 2,
+                                   lapply(26:28, function(i) {
+                                     numericInput(inputId = paste0("logistic_input_V", i), label = paste0("V", i), value = round(VARIABLE_MEANS[i], digits = 3), step = 0.1)
+                                   }),
+                                   numericInput(inputId = "logistic_input_amount", label = "Amount", value = round(VARIABLE_MEANS[29], digits = 3)),
+                                   br(), actionBttn(inputId = "logistic_reset", label = "Reset", color = "success")
+                                 )
+                               ),
+                               box(
+                                 title = "Results",
+                                 column(
+                                   width = 4,
+                                   uiOutput(outputId = "logistic_result"),
+                                   selectInput(inputId = "logistic_fixVariable", label = "Regressor to Fix", choices = LOGISTIC_FIXED_VARIABLE_CHOICES,
+                                               selected = 1)
+                                 ),
+                                 column(
+                                   width = 8,
+                                   plotOutput(outputId = "logistic_plot")
+                                 )
+                               )
+                             )
+                           )
                          )
-                         #####
                        ),
                        tabItem(
                          tabName = "summary",
@@ -148,7 +272,7 @@ ui = dashboardPage(skin = "black",
 )
 
 
-server = function(input, output) {
+server = function(input, output, session) {
   output$image_introduction = renderImage(expr = {
     list(
       src = paste0("www/images/introduction.png"),
@@ -168,17 +292,50 @@ server = function(input, output) {
   }, deleteFile = FALSE)
   
   output$dataTable = DT::renderDataTable(expr = {
-    datasetFormatted
+    DATASET_TRANSACTIONS
   })
   
-  # output$image_architecture = renderImage(expr = {
-  #   list(
-  #     src = paste0("www/images/architecture.png"),
-  #     contentType = "image/png",
-  #     width = APP_WIDTH,
-  #     height = APP_HEIGHT
-  #   )
-  # }, deleteFile = FALSE)
+  output$image_correlationMatrix = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/corrmatrix.png"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
+  
+  output$image_overTime = renderUI(expr = {
+    # imageSource = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/plotly_figures/EDA/Transactions+Over+Time%3A+Legitimate+and+Fraudulent.html"
+    # tags$img(src = imageSource)
+    includeHTML(path = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/plotly_figures/EDA/Transactions+Over+Time%3A+Legitimate+and+Fraudulent.html")
+  })
+  
+  output$image_classDistribution = renderUI(expr = {
+    includeHTML(path = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/plotly_figures/EDA/Distribution+of+Transaction+Amounts%3A+Legitimate+and+Fradulent.html")
+  })
+  
+  output$image_architecture = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/Cloud+Infrastructure+Updated.PNG"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
+  
+  output$image_recall = renderUI(expr = {
+    includeHTML(path = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/plotly_figures/precision-recall/Recall%2BPrecision_2.html")
+  })
+  
+  output$image_recallAndPrecision = renderUI(expr = {
+    includeHTML(path = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/plotly_figures/precision-recall/Recall%2BPrecision_1.html")
+  })
+  
+  output$image_randomForest_confusionMatrix = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/ranfor_confmatrix.png"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
+  
+  output$image_randomForest_roc = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/Random_Forest_ROC_Curve.png"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
   
   output$image_decisionTrees = renderUI(expr = {
     imageUrl = paste0("https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/creditcard_",
@@ -187,49 +344,122 @@ server = function(input, output) {
     tags$img(src = imageUrl, alt = "Image not available at this time!")
   })
   
-  # temp
-  #####
-  output$modelVisual2_1 = renderUI(expr = {
+  output$image_logistic_confusionMatrix = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/logreg_confmatrix.png"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
+  
+  output$image_logistic_roc = renderUI(expr = {
+    imageUrl = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/Logistic_ROC_Curve.png"
+    
+    tags$img(src = imageUrl, alt = "Image not available at this time!")
+  })
+  
+  output$logistic_overview = renderUI(expr = {
     withMathJax(
-      "$$y_i = malignance \\, of \\, cancer \\, case \\, i$$",
-      "$$x_{1,i} = max \\, concave \\, points \\, of \\, cancer \\, case \\, i$$",
-      "$$x_{2,i} = texture \\, of \\, cancer \\, case \\, i \\, (grayscale \\, deviation)$$",
-      "$$x_{3,i} = max \\, radius \\, of \\, cancer \\, case \\, i$$",
-      "$$logistic(x) = \\frac{1}{1 + e^{-x}}$$"
+      "$$k = \\# \\, variables$$",
+      "$$\\mu = probability \\, of \\, fraud$$",
+      "$$y = fraud \\, estimate \\, (1 = fraud, \\, 0 = not \\, fraud)$$",
+      "$$variables: X_1,...,X_k$$",
+      "$$coefficients: \\beta_0,\\beta_1,...,\\beta_k$$",
+      "$$log \\, odds \\, ratio: ln\\left(\\frac{\\mu}{1-\\mu}\\right) = \\beta_0 + \\sum_{j=1}^k \\beta_j X_j = x$$",
+      "$$\\mu = \\frac{1}{1+e^{-x}}$$",
+      "$$y = \\begin{cases} 1, \\, \\mu\\geq0.5 \\\\ 0, \\, \\mu<0.5 \\end{cases}$$"
     )
   })
   
-  output$modelVisual2_2 = renderUI(expr = {
-    withMathJax(
-      "$$\\hat{\\beta}_0 = -34.58$$",
-      "$$\\hat{\\beta}_1 = 57.71$$",
-      "$$\\hat{\\beta}_2 = 0.306$$",
-      "$$\\hat{\\beta}_3 = 1.154$$",
-      "$$\\hat{\\alpha} = 0.0001$$"
+  output$dataTable_logisticCoefficients = renderDataTable(expr = {
+    datatable(DATASET_LOGISTIC_COEFFICIENTS %>% mutate(Coefficient = round(Coefficient, digits = 3)),
+              rownames = NULL,
+              options = list(
+                pageLength = 7,
+                searching = FALSE,
+                lengthChange = FALSE
+              )
     )
   })
   
-  output$logOdds = renderUI(expr = {
+  observeEvent(eventExpr = input$logistic_reset, handlerExpr = {
+    
+    lapply(1:28, function(i) {
+      updateNumericInput(session = session, inputId = paste0("logistic_input_V", i), value = round(VARIABLE_MEANS[i][[1]], digits = 3))
+    })
+    
+    updateNumericInput(session = session, inputId = "logistic_input_amount", value = round(VARIABLE_MEANS[29][[1]], digits = 3))
+  })
+  
+  output$logistic_result = renderUI(expr = {
+    logit_estimate = LOGISTIC_BETA_INTERCEPT
+    for(i in 1:28) {
+      logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[i] * input[[paste0("logistic_input_V", i)]]
+      # print(input[[paste0("logistic_input_V", i)]])
+    }
+    logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[29] * input[["logistic_input_amount"]]
+    mu_estimate = 1/(1+exp(-logit_estimate))
+    fraud_estimate = if(is.na(mu_estimate)) {
+      NA
+    } else if(mu_estimate>=0.5) {
+      1
+    } else {
+      0
+    }
+    
     withMathJax(
       sprintf(
-        "$$x = \\hat{\\beta}_0 + %.2f  \\hat{\\beta}_1 + %.0f  \\hat{\\beta}_2 + %.0f  \\hat{\\beta}_3 = %.2f$$",
-        input$x1, input$x2, input$x3, -34.58+57.71*input$x1+0.306*input$x2+1.154*input$x3
+        "$$\\hat \\mu = %.3f$$",
+        mu_estimate
       ),
       sprintf(
-        "$$\\hat{\\mu} = \\frac{1-\\alpha}{1+e^{-x}} + \\frac{1}{2}\\alpha = %.2f$$",
-        (1-0.0001)/(1+exp(-(-34.58+57.71*input$x1+0.306*input$x2+1.154*input$x3))) + 0.5*0.0001
-      ),
-      sprintf(
-        "$$y=%.0f$$",
-        if((1-0.0001)/(1+exp(-(-34.58+57.71*input$x1+0.306*input$x2+1.154*input$x3))) + 0.5*0.0001 > 0.5) {
-          1
-        } else {
-          0
-        }
+        "$$\\hat y = %i$$",
+        fraud_estimate
       )
     )
   })
-  #####
+  
+  output$logistic_plot = renderPlot(expr = {
+    if(input$logistic_fixVariable == "29") {
+      x_coord = -5000:5000
+      logit_estimate = LOGISTIC_BETA_INTERCEPT
+      for(i in 1:28) {
+        logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[i] * input[[paste0("logistic_input_V", i)]]
+      }
+      logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[29] * x_coord
+      mu_estimate = 1/(1+exp(-logit_estimate))
+      
+      ggplot(mapping = aes(x = x_coord, y = mu_estimate)) +
+        geom_line() +
+        scale_x_continuous(breaks = -5:5*1000, labels = -5:5*1000) +
+        scale_y_continuous(breaks = 0:10/10, labels = 0:10/10) +
+        xlab(label = "Amount") +
+        ylab(label = "Probability of Fraud")
+    } else {
+      x_coord = -100:100
+      logit_estimate = LOGISTIC_BETA_INTERCEPT
+      for(i in 1:28) {
+        if(as.numeric(input$logistic_fixVariable) == i) {
+          NULL
+        } else {
+          logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[i] * input[[paste0("logistic_input_V", i)]]
+        }
+      }
+      logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[29] * input[["logistic_input_amount"]]
+      logit_estimate = logit_estimate + DATASET_LOGISTIC_COEFFICIENTS$Coefficient[as.numeric(input$logistic_fixVariable)] * x_coord
+      mu_estimate = 1/(1+exp(-logit_estimate))
+      
+      ggplot(mapping = aes(x = x_coord, y = mu_estimate)) +
+        geom_line() +
+        scale_x_continuous(breaks = -5:5*20, labels = -5:5*20) +
+        scale_y_continuous(breaks = 0:10/10, labels = 0:10/10) +
+        xlab(label = paste0("V", input$logistic_fixVariable)) +
+        ylab(label = "Probability of Fraud")
+    }
+    
+  })
+  
+  # observeEvent(eventExpr = input$logistic_fixVariable, handlerExpr = {
+  #   print(input$logistic_fixVariable)
+  # })
 }
 
 shinyApp(ui, server)
--- Credit_Card_Fraud/global.R
+++ Credit_Card_Fraud/global.R
@@ -4,22 +4,38 @@
 library(data.table) # fast read files
 library(dplyr) # data manipulation
 library(DT) # render data tables
+library(ggplot2) # better graphs
 library(RCurl) # used to download s3 data file
 library(shiny) # required
+library(shinycssloaders) # used for loading spinners
 library(shinydashboard) # dashboard look
+library(shinyWidgets) # fancier ui components
 
 #####
 
-# data read
+# data reads
 #####
-# dataset = fread(file = "www/dataset/creditcard.csv", sep = ",", header = TRUE)
-# dataset = fread(file = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/creditcardraw.csv", sep = ",", header = TRUE)
 download = getURL(url = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/creditcardraw.csv?accessType=DOWNLOAD")
 dataset = fread(text = download)
-datasetFormatted = dataset[,c(1,31,30,2:29)] %>% round(digits = 2)
+DATASET_TRANSACTIONS = dataset[,c(1,31,30,2:29)] %>% round(digits = 2)
+VARIABLE_MEANS = sapply(X = DATASET_TRANSACTIONS[,-c(1,2)], FUN = mean)
+VARIABLE_MEANS = VARIABLE_MEANS[c(2:29,1)]
+
+download = getURL(url = "https://cloudcomputinggroup10data.s3.us-east-2.amazonaws.com/sagemaker/figures/coefficients.csv?accessType=DOWNLOAD")
+DATASET_LOGISTIC_COEFFICIENTS = fread(text = download, skip = 1, drop = 1, col.names = c("Variable","Coefficient"))
 #####
 
 # global variables
 #####
 APP_WIDTH = 1400
 APP_HEIGHT = 800
+
+x = c(
+  "V1" = 1,
+  "V2" = 2
+)
+
+LOGISTIC_FIXED_VARIABLE_CHOICES = 1:29
+names(LOGISTIC_FIXED_VARIABLE_CHOICES) = c(paste0("V", 1:28), "Amount")
+
+LOGISTIC_BETA_INTERCEPT = 0 # update from Spencer
